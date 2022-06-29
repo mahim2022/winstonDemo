cc943a21e9a60a7eedd3abeb92d8e170a95e510c
mahim2022 <naimulmushfiq98@gmail.com>
2022-06-29 14:05:37 +0600

    First Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//https://www.youtube.com/watch?v=m2q1Cevl_qw    ---Source Video
+
+const logger = require("./logger/index");
+require("dotenv").config();
+
+// console.log(process.env);
+
+logger.warn("Warning");
